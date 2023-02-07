28ffbfad7b1c25bf2a8e06c22b4a414cc2b3f8dc
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-02-06 21:28:56 -0800

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/chat' element={<Search renderTweets={renderTweets}/>}></Route>
+          </Routes>
+        </div>
+    </Router>
   );
 }
 
