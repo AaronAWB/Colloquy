e4f69197dce0286a09b3c0b8bd13c242a31f583b
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-02-26 11:28:42 -0800

    shift submit button to side of input field

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,5 +1,27 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
 import '@Styles/Chat.css'
+
+let userSelf = {
+    name: 'AaronAWB',
+    messages: [
+        { text: "Testing! Testing!"},
+        { text: "This is me!"}
+    ]
+}
+
+let userAndy = {
+    name: 'AndyS',
+    messages: [
+        { text: "Andy1"},
+        { text: "Andy2"}
+    ]
+}
+
+const users = [userSelf, userAndy]
+
+function renderMessage (user, message) {
+    return <p>{user.name}: {message}</p>
+}
 
 function Chat () {
     return (
@@ -8,25 +30,25 @@ function Chat () {
             <Col md={3}>
                 <div className='users-container rounded bg-light p-3 mt-5 d-flex flex-column'>
                     <h2>Users</h2>
-                    <ul className='flex-grow-1'>
-                        
-                    </ul>
+                    <div className='flex-grow-1'>
+            
+                    </div>
                 </div>
             </Col>
             <Col md={9}>
                 <div className='chat-container rounded bg-white p-3 mb-3 mt-5'>
                     <h2>Chat</h2>
                     <div>
-                        
+                        {renderMessage(userAndy, userAndy.messages[0].text)}
+                        {renderMessage(userSelf, userSelf.messages[0].text)}
+                        {renderMessage(userSelf, userSelf.messages[1].text)}
                     </div>
                 </div>
                 <div className='message-input-container rounded bg-light p-3'>
-                    <Form>
-                        <Form.Group className='mb-3'>
+                    <InputGroup>
                         <Form.Control type='text' id='messageInput' />
-                        </Form.Group>
                         <Button type='submit' variant='primary'>Send</Button>
-                    </Form>
+                    </InputGroup>
                 </div>
             </Col>
             </Row>
