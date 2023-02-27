76f952674ab414b5f28063c609d5b25f4147bfc6
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-02-26 18:33:43 -0800

    add serve_index route to server

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 def create_app():
-    app = Flask(__name__, static_folder='../build', static_url_path='/')
+    app = Flask(__name__, static_folder='../static/dist', static_url_path='')
+
+    @app.route('/')
+    def serve_index():
+        return send_from_directory(app.static_folder, 'index.html')
 
     return app
+
