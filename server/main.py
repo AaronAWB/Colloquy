3a910940090a4ff94a92ae279e8f28ad619cef94
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-03 21:21:08 -0800

    add functioning messages endpoints with dummy data

--- server/main.py
+++ server/main.py
@@ -1,14 +1,28 @@
+from flask import Flask, request
 from src import create_app
 
 app = create_app()
 
-@app.get('/api/messages')
-def return_current_messages():
-    return 'Message returned.'
+messages = [
+    {
+        "User": "AaronAWB",
+        "Message": "This is a test message."
+    },
+    {
+        "User": "SecondUser",
+        "Message": "This is a message in response to the test message"
+    }
+]
 
-@app.post('/api/messages')
+@app.route('/api/messages', methods = ['GET'])
+def return_current_messages():
+    return messages, 200
+
+@app.route('/api/messages', methods = ['POST'])
 def create_message():
-    return 'Message created.'
+    request_data = request.json
+    messages.append(request_data)
+    return 'Message added', 200
 
 @app.get('/api/users')
 def get_users():
