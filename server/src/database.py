558ef8ccaf30ad76617cb8e6d267dae62966b3b4
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-06 14:55:49 -0800

    add get_table database function

--- server/src/database.py
+++ server/src/database.py
@@ -1,10 +1,42 @@
+from flask import jsonify
 import psycopg2
-import psycopg2.extras
 
 from config import config
 
+def get_table (table):
+    try: 
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        sql_query = f'SELECT * FROM {table}'
+
+        cur.execute(sql_query)
+        results = cur.fetchall()
+
+        cur.execute (f'SELECT count(*) FROM information_schema.columns WHERE table_name = {table}')
+        columns_num = cur.fetchone()[0]
+
+        cur.close()
+        conn.close
+
+        results_list = []
+        for row in results:
+            row_dict = {}
+            for i in range(columns_num):
+                row_dict[cur.description[i].name] = row[i]
+            results_list.append(row_dict)
+    
+        return jsonify(results_list) 
+    
+    except Exception as e:
+        return jsonify(f'Error: {str(e)}'), 500
+
 def connect():
+    
     conn = None
+    cur = None
+    
     try:
         params = config()
         print('Connecting to the PostgreSQL server...')
@@ -13,8 +45,10 @@ def connect():
 
         cur = conn.cursor()
         cur.close()
+    
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
+    
     finally:
         if conn is not None:
             conn.close()
