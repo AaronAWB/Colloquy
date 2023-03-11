f7019ea40ebcafa17a3b3886a544ba1b9ba9de72
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-11 12:23:27 -0800

    add DB_Connection class

--- server/src/lib/db.py
+++ server/src/lib/db.py
@@ -1,56 +1,69 @@
+import requests
+import os
 from flask import Flask, jsonify
 import psycopg2
 
-from config import config
+from dotenv import load_dotenv; load_dotenv()
 
-app = Flask(__name__)
+class DB_Connection():
+
+    def __init__(self) -> None:
+        self.session = requests.Session()
+        self.session.body = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            }
+        self.connection_params = {os.getenv()}
+
+
+# app = Flask(__name__)
     
-def get_table(table):
-    with app.app_context():
-        try: 
+# def get_table(table):
+#     with app.app_context():
+#         try: 
 
-            params = config()
-            print('Connecting to the PostgreSQL server...')
-            conn = psycopg2.connect(**params)
-            print('Connection established.')
+#             params = config()
+#             print('Connecting to the PostgreSQL server...')
+#             conn = psycopg2.connect(**params)
+#             print('Connection established.')
         
-            cur = conn.cursor()
+#             cur = conn.cursor()
 
-            sql_query = f'SELECT * FROM {table}'
+#             sql_query = f'SELECT * FROM {table}'
 
-            cur.execute(sql_query)
-            results = cur.fetchall()
+#             cur.execute(sql_query)
+#             results = cur.fetchall()
 
-            cur.close()
-            conn.close()
-            print('Database connection closed')
+#             cur.close()
+#             conn.close()
+#             print('Database connection closed')
     
-            return jsonify(results) 
+#             return jsonify(results) 
         
-        except(Exception, psycopg2.DatabaseError) as error:
-            print(error)
+#         except(Exception, psycopg2.DatabaseError) as error:
+#             print(error)
 
-def add_message(user_id, text, date):
-    with app.app_context():
-        try:
+# def add_message(user_id, text, date):
+#     with app.app_context():
+#         try:
 
-            params = config()
-            conn =  psycopg2.conntect(**params)
+#             params = config()
+#             conn =  psycopg2.conntect(**params)
 
-            cur = conn.cursor
-            sql_query = f'INSERT INTO messages ({user_id}, {text}, {date})'
-            cur.execute(sql_query)
+#             cur = conn.cursor
+#             sql_query = f'INSERT INTO messages ({user_id}, {text}, {date})'
+#             cur.execute(sql_query)
             
-            cur.close()
-            conn.close()
+#             cur.close()
+#             conn.close()
 
-        except(Exception, psycopg2.DatabaseError) as error:
-            print(error)
+#         except(Exception, psycopg2.DatabaseError) as error:
+#             print(error)
 
-print(f'The get_table function returned: {get_table("messages")}')
+# print(f'The get_table function returned: {get_table("messages")}')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
     
 
 
