112f15ec101bbb944f0a289009fdffbf748a6c32
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-11 13:10:21 -0800

    add CreateUser endpoint

--- server/src/routes/database/__init__.py
+++ server/src/routes/database/__init__.py
@@ -9,12 +9,12 @@ api = Api(api_bp)
 @api.route('/messages')
 class GetAllMessages(Resource):
     def get(self):
-        return DB_Connection.get_table('messages')
+        return DB_Connection.get_table('messages'), 200
 
 @api.route('/users')
 class CreateMessage(Resource):
     def get(self):
-        return DB_Connection.get_table('users')
+        return DB_Connection.get_table('users'), 200
     
 @api.route('/messages')
 class CreateMessage(Resource):
@@ -23,4 +23,12 @@ class CreateMessage(Resource):
         user_id = data["user_id"]
         text = data["text"]
         created_date = data["created_date"]
-        return DB_Connection.add_message(user_id, text, created_date)
+        return DB_Connection.add_message(user_id, text, created_date), 201
+    
+@api.route('/users')
+class CreateUser(Resource):
+    def post(self):
+        data = Request.get_json()
+        username = data["username"]
+        user_id = data["user_id"]
+        return DB_Connection.add_user(username, user_id), 201
