59ef26066a76c7a1f41f9152e9c8e88416c4572e
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-19 16:46:00 -0700

    add styling to Chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -10,23 +10,31 @@ function Chat () {
         <Container>
             <Row>
             <Col md={3}>
-                <div className='users-container rounded bg-light p-3 mt-5 d-flex flex-column'>
-                    <h2>Users</h2>
+                <div className='users-container rounded shadow p-3 mt-5 d-flex flex-column'>
+                    <h2>Channels</h2>
                     <div className='flex-grow-1'>
             
                     </div>
                 </div>
             </Col>
-            <Col md={9}>
-                <div className='chat-container rounded bg-white p-3 mb-3 mt-5'>
+            <Col md={6}>
+                <div className='chat-container rounded shadow p-3 mb-3 mt-5'>
                     <h2>Chat</h2>
-                    
+                    <div className = 'message-bar-container'>
+                        <InputGroup>
+                            <Form.Control className='message-bar' type='text' id='messageInput' />
+                            <Button type='submit' variant='primary'>Send</Button>
+                        </InputGroup>
+                    </div>
                 </div>
-                <div className='message-input-container rounded bg-light p-3'>
-                    <InputGroup>
-                        <Form.Control type='text' id='messageInput' />
-                        <Button type='submit' variant='primary'>Send</Button>
-                    </InputGroup>
+
+            </Col>
+            <Col md={3}>
+                <div className='users-container rounded shadow p-3 mt-5 d-flex flex-column'>
+                    <h2>Users</h2>
+                    <div className='flex-grow-1'>
+            
+                    </div>
                 </div>
             </Col>
             </Row>
