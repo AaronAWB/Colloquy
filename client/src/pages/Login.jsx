e4c92d316f8f27935ad4d980eaa54e1c906667b2
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-03-21 20:53:50 -0700

    add Login page styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,30 @@
+import { Container, Form, Button } from 'react-bootstrap';
+import '@Styles/Login.css'
+
 function Login () {
     return (
-        <div>
-            <p>This is the login page!</p>
-            <a href='/chat'>Go to Chat</a>
-            <p>Testing</p>
-        </div>
-    )
+        <Container className='content-container'> 
+            <Container className="login-box rounded shadow">
+            <div className='login-title'>
+                <h2>Login</h2>
+            </div>
+            <Form className='mt-3'>
+                <Form.Group controlId="formBasicUsername" className='mt-3'>
+                    <Form.Label className ='form-lable'>Username:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username"  />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword" className='mt-3'>
+                    <Form.Label className='form-lable'>Password:</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" />
+                </Form.Group>
+            <Button className='mt-3' variant="primary" type="submit">Login</Button>
+            </Form>
+            <div className='signup-prompt'>
+                <p>Don't have an account? <a>Sign up</a></p>
+            </div>
+        </Container>
+    </Container>
+  );
 }
 
 export { Login } 
