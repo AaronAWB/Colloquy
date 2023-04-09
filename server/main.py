ed49891f9f01b4f6294ccc86b081d30b9af35738
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-04-08 17:16:55 -0700

    remove second instance of socketio in socketio.py

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
-from src import create_app
+from flask import request
 from flask_socketio import SocketIO
+from src import create_app
 
 app = create_app()
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*', logger=True)
+
+@socketio.on('connect')
+def handle_connection():
+    print("----- USER CONNECTED ---------: ", request.sid)
+
+@socketio.on('test')
+def test(data):    
+    print(data)
 
 print(app.url_map)
 
--- server/src/routes/socketio.py
+++ server/src/routes/socketio.py
@@ -1,12 +1,14 @@
 from flask import request
-from flask_socketio import SocketIO, emit
+from main import socketio
 from src.lib.db_connection import db_connection
 
-socketio = SocketIO()
+@socketio.on('connect')
+def handle_connection():
+    print("----- USER CONNECTED ---------: ", request.sid)
 
 @socketio.on('test')
-def test():    
-    print("TEST EVENT FIRED!Connected")
+def test(data):    
+    print(data)
 
 @socketio.on('connect')
 def handle_connection():
@@ -20,19 +22,19 @@ def handle_add_message(data):
     userId = data['userId']
     message = db_connection.add_message(messageContent, channel, userId)
     print(message)
-    emit('message_added', message, broadcast=True)
+    socketio.emit('message_added', message, broadcast=True)
 
 @socketio.on('update_messages')
 def handle_update_messages(data):
     messages = db_connection.get_table(data['channel'])
-    emit('updated_message_list', messages, broadcast=True)
+    socketio.emit('updated_message_list', messages, broadcast=True)
 
 @socketio.on('update_channel_list')
 def handle_update_channels():
     channels = db_connection.get_table('channels')
-    emit('updated_channel_list', channels, broadcast=True)
+    socketio.emit('updated_channel_list', channels, broadcast=True)
 
 @socketio.on('add_channel')
 def handle_add_channel(data):
     new_channel = db_connection.add_channel(data)
-    emit ('channel_added', new_channel, broadcast=True)
+    socketio.emit ('channel_added', new_channel, broadcast=True)
