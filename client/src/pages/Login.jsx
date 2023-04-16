15a8ba26b594a2b65592546cd4f11a3af6962f77
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-04-15 23:23:28 -0700

    add invalid login alert

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
 import { SignUp } from '@Components/index';
 import { AuthMethods, socketConnect } from '@Utils/index';
 import '@Styles/Login.css'
@@ -10,6 +10,7 @@ function Login () {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [showModal, setShowModal] = useState(false);
+    const [loginError, setLoginError] = useState(false);
 
     const auth = new AuthMethods();
     const navigate = useNavigate();
@@ -18,6 +19,10 @@ function Login () {
         if (auth.loggedIn()) {navigate('/')};
     }, []);
 
+    useEffect(() => {
+        console.log(`Login error state is: ${loginError}`)
+    }, [loginError])
+
     const handleLogin = async e => {
         e.preventDefault();
         try {
@@ -25,10 +30,14 @@ function Login () {
             if (login === true) {
                 navigate('/')
                 socketConnect();
+            } else {
+                setLoginError(true);
+                setUsername("");
+                setPassword("");
             }
         } catch (err) {
             console.log(`Login failed: ${err}`)
-            }
+            };
     };
     
     const handleGuestLogin = async e => {
@@ -48,45 +57,56 @@ function Login () {
     const handleHideModal = () => {setShowModal(false)};
         
     return (
-        <Container className='content-container'> 
-            <Container className="login-box rounded shadow">
-            <div className='login-title'>
-                <h2>Login</h2>
-            </div>
-            <Form className='mt-3' onSubmit={handleLogin}>
-                <Form.Group controlId='formBasicUsername' className='mt-3'>
-                    <Form.Control 
-                        className='text-center username-input shadow' 
-                        type='text' 
-                        placeholder="Username" 
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        />
-                </Form.Group>
-                <Form.Group controlId='formBasicPassword' className='mt-3'>
-                    <Form.Control 
-                        className='text-center password-input shadow'
-                        type='password' 
-                        placeholder="Password" 
-                        value={password} 
-                        onChange={e => setPassword(e.target.value)} 
-                        />     
-                </Form.Group>
-                <div className ='button-container'>
-                    <Button className='mt-3 login-button shadow' type='submit'>Sign In</Button>
+        <Container className='content-container'>
+            <div className='login-container'>
+                <Container className='alert-container'>
+                    {loginError && (
+                    <Alert className='login-alert' variant="danger" onClose={() => setLoginError(false)} dismissible>
+                        <p>
+                            Invalid username or password. Please try again.
+                        </p>
+                    </Alert>
+                )}
+                </Container> 
+                <Container className="login-box rounded shadow">
+                <div className='login-title'>
+                    <h2>Login</h2>
                 </div>
-            </Form>
-            <div className='signup-prompt'>
-                <p>Don't have an account? <a href='#' onClick={handleShowModal}>Sign up</a></p>
-                <SignUp 
-                    show={showModal}
-                    hide={handleHideModal}
-                    />
-            </div>
-            <div className='guest-prompt'>
-                <p>Continue as <a href='#' onClick={handleGuestLogin}>Guest</a></p>
-            </div>
-        </Container>
+                <Form className='mt-3' onSubmit={handleLogin}>
+                    <Form.Group controlId='formBasicUsername' className='mt-3'>
+                        <Form.Control 
+                            className='text-center username-input shadow' 
+                            type='text' 
+                            placeholder="Username" 
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            />
+                    </Form.Group>
+                    <Form.Group controlId='formBasicPassword' className='mt-3'>
+                        <Form.Control 
+                            className='text-center password-input shadow'
+                            type='password' 
+                            placeholder="Password" 
+                            value={password} 
+                            onChange={e => setPassword(e.target.value)} 
+                            />     
+                    </Form.Group>
+                    <div className ='button-container'>
+                        <Button className='mt-3 login-button shadow' type='submit'>Sign In</Button>
+                    </div>
+                </Form>
+                <div className='signup-prompt'>
+                    <p>Don't have an account? <a href='#' onClick={handleShowModal}>Sign up</a></p>
+                    <SignUp 
+                        show={showModal}
+                        hide={handleHideModal}
+                        />
+                </div>
+                <div className='guest-prompt'>
+                    <p>Continue as <a href='#' onClick={handleGuestLogin}>Guest</a></p>
+                </div>
+            </Container>
+        </div>
     </Container>
   );
 }
